1b7a38d7e3dc0475e6acb7e8e621ac2ff1631168
Antonio Santos Hernandez <toncho1026@hotmail.com>
2025-10-08 19:57:51 -0600

    Se agrega exception a conexion de BDs

--- src/main/java/com/reactive/exception/GlobalExceptionHandler.java
+++ src/main/java/com/reactive/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.reactive.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,14 +23,20 @@ public class GlobalExceptionHandler {
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "INTERNAL SERVER ERROR", ex);
     }
 
+    @ExceptionHandler(DataAccessResourceFailureException.class)
+    public ResponseEntity<ErrorResponse> handleMongoConnectionError(DataAccessResourceFailureException ex) {
+        return buildErrorResponse(HttpStatus.SERVICE_UNAVAILABLE.value(),
+                "Base de datos no disponible.", ex);
+    }
+
     public ResponseEntity<ErrorResponse> buildErrorResponse(int status, String message, Exception ex) {
         ErrorResponse errorResponse = new ErrorResponse();
 
         errorResponse.setStatusCode(status);
         errorResponse.setMessageError(message);
-        errorResponse.setDescription(ex.getMessage());
+        errorResponse.setDescription(status == 503 ? "Timed out while waiting for a server." : ex.getMessage());
 
-        LOGGER.error("exception: {}", errorResponse);
+        LOGGER.error("exception: {}", errorResponse, ex);
         return ResponseEntity.status(status).body(errorResponse);
     }
 
